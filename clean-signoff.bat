26280c41d64f8b7f9c7c67b369f2fe6821ec4880
lI-Isekai-Il <182264440+lI-Isekai-Il@users.noreply.github.com>
2025-11-25 14:01:00 +0300

    Removed clean-signoff.bat and updated .gitignore

--- clean-signoff.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Removing Signed-off-by lines from history...
-git filter-repo --message-callback "import re; return re.sub(br'Signed-off-by:.*', b'', message)" --force
-git push --force --all
-git push --force --tags
-echo Done.
-pause
